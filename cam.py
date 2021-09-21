fccffaf4c0ab3739ab67476802d608a6d39e8e60
mmaatthieuu <matthieu.schmidt@epfl.ch>
2021-09-21 17:16:25 +0200

    adding existing files

---
+++ cam.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+import pathlib
+
+import numpy as np
+import time
+import picamera
+import argparse
+import os.path
+
+import NPImage as npi
+
+
+
+import cam_lib as cl
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+parser.add_argument("-vv", "--vverbose", help="increase more output verbosity",
+                    action="store_true")
+parser.add_argument("-p", "--preview", help="display camera preview",
+                    action='store_true')
+parser.add_argument("-t", "--timeout", help="time (in s) before takes picture and shuts down",
+                    default=10, nargs='?', type=int)
+parser.add_argument("-ti", "--time-interval", help="time interval between frames in seconds",
+                    default=5, nargs='?', type=int)
+parser.add_argument("-avg", "--average", help="number of pictures to average at each frame",
+                    default=3, nargs='?', type=int)
+parser.add_argument("-o","--output", help="output filename",
+                    nargs=1, type=str)
+parser.add_argument("-q", "--quality", help="set jpeg quality <0 to 100>",
+                    nargs='?', type=int, default=75)
+parser.add_argument("-iso", "--iso", help="sets the apparent ISO setting of the camera",
+                    nargs='?', type=int, default=100)
+parser.add_argument("-ss", "--shutter-speed", help="sets the shutter speed of the camera in microseconds",
+                    nargs='?', type=int, const=0)
+parser.add_argument("-x", "--compress", help="",
+                    nargs='?', type=int, const=1000)
+
+args = parser.parse_args()
+
+
+local_tmp_dir = ".campy_local_save/"
+absolute_output_folder = pathlib.Path(cl.get_folder_name(args.output[0])).absolute()
+output_filename = cl.get_file_name(args.output[0])
+
+# print(absolute_output_folder)
+# print(output_filename)
+
+
+
+
+def init():
+
+    if args.vverbose:
+        args.verbose = True
+
+    if args.verbose:
+        cl.print_args(args)
+
+    if args.compress is not None:
+        try:
+            os.mkdir(local_tmp_dir)
+            print("Folder ", local_tmp_dir, " created")
+        except FileExistsError:
+            pass
+            # os.remove(".campy_local_save/*")
+        os.chdir(local_tmp_dir)
+
+
+
+
+def main():
+    init()
+
+    nPicsPerFrames = args.average
+    n_frames_total = args.timeout // args.time_interval
+
+    try:
+        delay = 0
+        with picamera.PiCamera(resolution='3296x2464') as camera:
+            camera.iso = args.iso
+
+            if args.preview:
+                camera.start_preview()
+
+            if args.verbose:
+                print("Starting camera...")
+            time.sleep(2)  # let the camera warm up and set gain/white balance
+
+            if args.shutter_speed is not None:
+                g = camera.awb_gains
+                camera.awb_mode = "off"
+                camera.awb_gains = g
+
+                camera.shutter_speed = args.shutter_speed
+                camera.exposure_mode = 'off'
+
+            if args.verbose:
+                print("Camera successfully started")
+                print("Current a/d gains: {}, {}".format(camera.analog_gain, camera.digital_gain))
+
+            for k in range(n_frames_total):
+                start_time = time.time()
+                pictures_to_average = np.empty((2464, 3296), dtype=np.uint8)
+
+                if args.vverbose:
+                    print("Starting capture of frame %d / %d" % (k+1, n_frames_total))
+                elif args.verbose:
+                    print("\rStarting capture of frame %d / %d" % (k + 1, n_frames_total), end="")
+
+                for i in range(nPicsPerFrames):
+                    output = npi.NPImage()
+                    try:
+                        camera.capture(output, 'yuv', use_video_port=False)
+
+                        pictures_to_average = pictures_to_average + output.get_data() // args.average
+                    except picamera.exc.PiCameraRuntimeError:
+                        print("Error 1 on frame %d" % i)
+                    except RuntimeError:
+                        print("Error 2 on frame %d" % i)
+
+                if args.output is not None:
+                    cl.save_image(pictures_to_average, k, absolute_output_folder,
+                                  output_filename, args.compress, n_frames_total, args.quality)
+
+
+                execTime = (time.time() - start_time)
+                if args.vverbose:
+                    print("Finished capture of frame %d in %fs" % (k + 1, execTime))
+
+                diff_time = args.time_interval - execTime
+                if diff_time - delay > 0:
+                    sleep_time = args.time_interval - execTime - delay
+                    if args.vverbose:
+                        print("Waiting for %fs" % sleep_time)
+                    time.sleep(sleep_time)
+                    delay = 0
+                else:
+                    delay -= diff_time
+                    if args.verbose:
+                        print('Frame %fs late' % -diff_time)
+                        print('Delay : %fs' % delay)
+    except KeyboardInterrupt:
+        print("\nScript interrupted by user")
+    # else:
+    #     print("\nOops... Something went wrong.\n")
+    finally:
+        if args.verbose:
+            print("Closing camera...")
+        camera.close()
+
+        if args.verbose:
+            print("Over.")
+
+if __name__ == "__main__":
+    main()
---
+++ cam_lib.py
@@ -0,0 +1,107 @@
+import pathlib
+
+import numpy as np
+from PIL import Image as im
+import threading
+import sys
+import shutil
+import tarfile
+import os.path
+
+
+def get_folder_name(in_string: str):
+    if '/' in in_string:
+        folder_name = "/".join(in_string.split('/')[0:-1])
+        # print(folder_name)
+        return folder_name
+    else:
+        print("Output files need to be in a folder")
+        # args.compress = None
+        return None
+
+
+def get_file_name(in_string: str):
+    return in_string.split('/')[-1]
+
+
+
+
+def save_image(picture_array, k, output_folder, output_filename, compress_step, n_frames_total, quality):
+    image = im.fromarray(picture_array)
+
+    try:
+        filename = output_filename % k
+    except TypeError:
+        filename = output_filename
+
+    if compress_step is None:
+        image.save(os.path.join(output_folder, filename), quality=quality)
+
+    else:
+        part = k // compress_step
+        current_dir = "part%d/" % part
+
+        if k % compress_step == 0:
+            try:
+                os.mkdir(current_dir)
+                print("\n%s created" % current_dir)
+            except FileExistsError:
+                print("\n%s already exists" % current_dir)
+
+        image.save(os.path.join(current_dir, filename), quality=quality)
+
+        if k % compress_step == compress_step - 1 or k == n_frames_total - 1:
+            print(threading.enumerate())
+            dir_to_compress = "part%d" % part
+            print("Dir_to_compress : %s" % dir_to_compress)
+            print("Dest path : %s " % output_folder)
+            compress_task = threading.Thread(target=compress, args=(dir_to_compress, output_folder))
+            compress_task.start()
+            print(threading.enumerate())
+
+
+def print_args(args):
+
+    nPicsPerFrames = args.average
+    n_frames_total = args.timeout // args.time_interval
+
+    h = args.timeout // 3600
+    mins = (args.timeout - h * 3600) // 60
+    s = args.timeout - h * 3600 - mins * 60
+
+    print("Verbosity turned on")
+    print("Taking frames every %ds" % args.time_interval)
+    print("Averaging %d pictures at each frame" % nPicsPerFrames)
+    print("Timeout : %ds (%dh%dmin%ds)" % (args.timeout, h, mins, s))
+    print("Capturing %d frames" % n_frames_total)
+    if args.output is not None:
+        print("Writing file to : %s" % args.output[0])
+    else:
+        print("Output files NOT saved to disk")
+    print("JPG quality set to : %d" % args.quality)
+    print("ISO : %d" % args.iso)
+    if args.shutter_speed is not None:
+        print("Shutter speed : %d" % args.shutter_speed)
+    else:
+        print("Shutter speed set automatically")
+
+    print(args.compress)
+
+    print("\n")
+
+
+def compress(folder_name, dest_path):
+    print("Starting compression of %s" % folder_name)
+    with tarfile.open(folder_name + ".tgz", "w:gz") as tar:
+        for file in os.listdir(pathlib.Path(folder_name)):
+            tar.add(os.path.join(folder_name, file), arcname=file)
+
+    try:
+        shutil.move(folder_name+".tgz", "%s/%s.tgz" % (dest_path, folder_name))
+        shutil.rmtree(folder_name)
+    except OSError:
+        print("Failed to move and/or delete folder")
+
+    print("Compression of %s done" % folder_name)
+
+
