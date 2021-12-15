521577180fc1a1e2a29f63eeaaf9003933aff790
Matthieu <matthieu.schmidt@epfl.ch>
2021-12-15 11:08:24 +0100

    function record instead of main

--- cam.py
+++ cam.py
@@ -15,6 +15,8 @@ import datetime
 
 from cam_lib import *
 
+from CrashTimeOutException import CrashTimeOutException
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
@@ -102,25 +104,77 @@ def save_info(args, version):
     return nfo_path
 
 
-
-def main():
-    init()
-    if args.save_nfo:
-        nfo_path = save_info(args, version)
+def record(args, camera, delay=0):
 
     nPicsPerFrames = args.average
     try:
         n_frames_total = args.timeout // args.time_interval
     except ZeroDivisionError:
         n_frames_total = 1
+
+    for k in range(args.start_frame, n_frames_total):
+        start_time = time.time()
+        pictures_to_average = np.empty((2464, 3296), dtype=np.uint8)
+
+        if args.vverbose:
+            log("Starting capture of frame %d / %d" % (k + 1, n_frames_total))
+        elif args.verbose:
+            print("\r[%s] : Starting capture of frame %d / %d" %
+                  (str(datetime.datetime.now()), k + 1, n_frames_total), end="")
+
+        for i in range(nPicsPerFrames):
+            output = npi.NPImage()
+            try:
+
+                camera.capture(output, 'yuv', use_video_port=False)
+                time.sleep(0.05)
+                # pictures_to_average = pictures_to_average + \
+                #                      cl.compressor(output.get_data(),args.r,args.th) // args.average
+                pictures_to_average = pictures_to_average + output.get_data() // args.average
+                # print(np.max(pictures_to_average))
+            except picamera.exc.PiCameraRuntimeError as error:
+                log("Error 1 on frame %d" % k)
+                log(error)
+                raise TimeoutError(k)
+                # sys.exit()
+            except RuntimeError:
+                log("Error 2 on frame %d" % k)
+
+        if args.output is not None:
+            save_image(pictures_to_average, k, absolute_output_folder,
+                       output_filename, args.compress, n_frames_total, args.quality, args.average, version)
+
+        execTime = (time.time() - start_time)
+        if args.vverbose:
+            log("Finished capture of frame %d in %fs" % (k + 1, execTime))
+
+        diff_time = args.time_interval - execTime
+        if diff_time - delay > 0:
+            sleep_time = args.time_interval - execTime - delay
+            if args.vverbose:
+                log("Waiting for %fs" % sleep_time)
+            time.sleep(sleep_time)
+            delay = 0
+        else:
+            delay -= diff_time
+            if args.verbose:
+                log('\nFrame %fs late' % -diff_time)
+                log('Delay : %fs' % delay)
+
+def main():
+    init()
+    if args.save_nfo:
+        nfo_path = save_info(args, version)
+
+
     try:
-        delay = 0
+
         subprocess.run(['cpulimit', '-P', '/usr/bin/gzip', '-l', '10','-b'])
         with picamera.PiCamera(resolution='3296x2464') as camera:
 
             cam_init(camera, iso=args.iso, shutter_speed=args.shutter_speed, brightness=args.brightness,
                      verbose=args.verbose)
-            #camera.CAPTURE_TIMEOUT = 10
+            #picamera.PiCamera.CAPTURE_TIMEOUT = 10
 
             if args.preview:
                 camera.start_preview()
@@ -138,59 +192,18 @@ def main():
                 log("Camera successfully started")
                 log(gain_str)
 
-            for k in range(args.start_frame, n_frames_total):
-                start_time = time.time()
-                pictures_to_average = np.empty((2464, 3296), dtype=np.uint8)
-
-                if args.vverbose:
-                    log("Starting capture of frame %d / %d" % (k+1, n_frames_total))
-                elif args.verbose:
-                    print("\r[%s] : Starting capture of frame %d / %d" %
-                          (str(datetime.datetime.now()),k + 1, n_frames_total), end="")
-
-                for i in range(nPicsPerFrames):
-                    output = npi.NPImage()
-                    try:
-
-                        camera.capture(output, 'yuv', use_video_port=False)
-                        time.sleep(0.05)
-                        #pictures_to_average = pictures_to_average + \
-                        #                      cl.compressor(output.get_data(),args.r,args.th) // args.average
-                        pictures_to_average = pictures_to_average + output.get_data()// args.average
-                        #print(np.max(pictures_to_average))
-                    except picamera.exc.PiCameraRuntimeError as error:
-                        log("Error 1 on frame %d" % k)
-                        log(error)
-                        sys.exit()
-                    except RuntimeError:
-                        log("Error 2 on frame %d" % k)
-
-                if args.output is not None:
-                    save_image(pictures_to_average, k, absolute_output_folder,
-                                  output_filename, args.compress, n_frames_total, args.quality, args.average, version)
+            record(args=args, camera=camera)
 
 
-
-                execTime = (time.time() - start_time)
-                if args.vverbose:
-                    log("Finished capture of frame %d in %fs" % (k + 1, execTime))
-
-                diff_time = args.time_interval - execTime
-                if diff_time - delay > 0:
-                    sleep_time = args.time_interval - execTime - delay
-                    if args.vverbose:
-                        log("Waiting for %fs" % sleep_time)
-                    time.sleep(sleep_time)
-                    delay = 0
-                else:
-                    delay -= diff_time
-                    if args.verbose:
-                        log('\nFrame %fs late' % -diff_time)
-                        log('Delay : %fs' % delay)
     except KeyboardInterrupt:
         log("\nScript interrupted by user")
     # else:
     #     log("\nOops... Something went wrong.\n")
+    except TimeoutError as e:
+        print("caught")
+        print(k)
+        #picamera.PiCamera.CAPTURE_TIMEOUT = 10
+
     finally:
 
         subprocess.run(['pkill', 'cpulimit'])
