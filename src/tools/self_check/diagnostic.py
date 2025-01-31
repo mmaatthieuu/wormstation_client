9ed019c625781c7301755fe0eeb263bb36323b7f
mmaatthieuu <matthieu.schmidt@epfl.ch>
2025-02-01 00:33:58 +0100

    remove debug print

--- src/tools/self_check/diagnostic.py
+++ src/tools/self_check/diagnostic.py
@@ -142,7 +142,6 @@ class Diagnostic:
 
             time.sleep(0.5)  # Allow time for the light to turn on
             image_path = f"test_{color}.jpg"
-            print(os.path.abspath(image_path))
             camera.capture_frame(image_path)
             time.sleep(0.5)
 
